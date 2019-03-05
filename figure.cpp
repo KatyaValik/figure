c04339226475cd64a6aab05115d53ed68a0c63c3
katerina <katya.valik@mail.ru>
2019-03-06 01:41:56 +0300

    somethind

--- figure.cpp
+++ figure.cpp
@@ -5,22 +5,48 @@
 #include <sstream>
 #include <vector>
 #include <algorithm>
+#include <math.h>
 
 using namespace std;
 
-class Figure{
+class Figure
+{
 public:
 	virtual double get_square()=0;
+	virtual ~Figure();
 };
 
-
-class Rectangle: public Figure{
+class Rectangle: public Figure
+{
+private:
+	double width;
+	double height;
 public:
-	virtual ~Rectangle();
 
-	static Figure * make(const string s);
+	Rectangle()
+	{
+		width = 0;
+		height = 0;
+	}
 
-	virtual double get_square();
+
+	virtual double get_square()
+	{
+		return width * height;
+	}
+
+	virtual ~Rectangle()
+	{
+		// что должен делать? все поля же интовые
+	}
+
+static	Figure * make(const string s) {
+		Rectangle r;
+		istringstream str;
+		r.width>>str;
+		r.height>>str;
+		return r;
+	}
 
 	string to_string()
 	{
@@ -30,48 +56,93 @@ public:
 		return str.str();
 	}
 
+};
+
+
+
+class Circle: public Figure
+{
 private:
-	Rectangle()
-{
-		width = 0;
-		height = 0;
-}
+	double radius;
 
-	double width;
-	double height;
-
-};
-
-static int Rectangle::size = 0;
-
-
-Rectangle::~Rectangle()
-{
-
-}
-
-Figure * makeFigure(const string s)
-{
-	if( s[0] == 'R' )
-		return Rectange::make(s);
-}
-
-class Circle: public Figure{
-	double width,height;
 public:
-	virtual double get_square();
+
+	Circle() {
+		radius=0;
+	}
+	virtual double get_square(){
+		return radius*radius*3.14;//как сделать нормальное пи? в math.h нет похоже
+	}
+	virtual ~Circle() {
+		//что делать?
+	}
+
+static	Figure* make(const string s){
+		Circle c;
+		istringstream str;
+		c.radius>>str;
+		return c;
+	}
+
+	string to_string(){
+		ostringstream str;
+		str<< "C "<<radius;
+		return str.str();
+	}
 };
 
-class Square: public Figure{
-	double width,height;
+class Square: public Figure
+{
+private:
+	double size;
 public:
-	virtual double get_square();
+	Square(){
+		size=0;
+	}
+	virtual double get_square(){
+		return size*size;
+	}
+	virtual ~Square(){
+		//чтооооо?
+	}
+
+static	Figure* make(const string s){
+		Square sq;
+		istringstream str;
+		sq.size>>str;
+		return sq;
+	}
+
+	string to_string(){
+		ostringstream str;
+		str<<"S "<<size;
+		return str.str();
+	}
 };
+
+Figure* makeFigure(const string s)
+{
+	if (s[0] == 'R')
+	{
+		return Rectangle::make(s);
+	}
+	if (s[0] == 'S')
+	{
+		return Square::make(s);
+	}
+
+	if (s[0] == 'C')
+	{
+		return Circle::make(s);
+	}
+
+}
 
 int main(int argc, char** argv)
 {
-	Figure* fig;
-
+/*	Figure* fig;
+	string str;
+	cin>>str;
 	fig = Rectangle::make(str);
-	cout << fig->to_string();
+	cout << fig->to_string();*/
 }
